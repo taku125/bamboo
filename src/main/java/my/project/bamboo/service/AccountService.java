9375f7ea581f544a08f0d2f0afed5751943525c0
taku125 <takuro1274@icloud.com>
2021-05-16 00:06:06 +0900

    DB接続設定＆アカウントEntity作成

---
+++ src/main/java/my/project/bamboo/service/AccountService.java
@@ -0,0 +1,29 @@
+package my.project.bamboo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import my.project.bamboo.entity.Account;
+import my.project.bamboo.repository.AccountRepository;
+
+@Service
+public class AccountService{
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public List<Account> findAll(){
+        return accountRepository.findAll();
+    }
+    public Account findOne(Long id) {
+        return accountRepository.getOne(id);
+    }
+    public Account save(Account account){
+        return accountRepository.save(account);
+    }
+    public void delete(Long id){
+        accountRepository.deleteById(id);
+    }
+}
